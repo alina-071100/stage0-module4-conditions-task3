66d0e5dc5c59cc0b6a01ba34bdff1102a8ed7f0b
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-18 13:59:14 +0400

    Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
-    public void amountOfDays(int month) {
-    }
+        public void amountOfDays(int month) {
+if (month > 12 || month <= 0){
+    System.out.println("wrong number!");
+}else if (month == 2 ){
+    System.out.println(28);
+}else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 9 || month == 11){
+    System.out.println(31);
+}else{
+    System.out.println(30);
 }
+        }
+    }
+
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        int getNumFromChar = symbol;
+        if ((getNumFromChar >= 65 && getNumFromChar <= 90) || (getNumFromChar >= 97 && getNumFromChar <= 122)) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }

c0e0373421086a4a2d92e449e98f2899bc5eb712
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-18 14:04:05 +0400

    Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
-    public void vowelDeterminer(char character) {
-
-    }
+        public void vowelDeterminer(char character) {
+            int getNumFromChar = character;
+            if (!((getNumFromChar >= 65 && getNumFromChar <= 90) || (getNumFromChar >= 97 && getNumFromChar <= 122))) {
+                System.out.println("wrong alphabet!");
+            } else if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        }
 }
